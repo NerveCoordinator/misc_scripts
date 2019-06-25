440a7213738dfed7faa40e966ac95e67aed2b165
Maximilian Van Hout <36654636+NerveCoordinator@users.noreply.github.com>
2019-06-24 22:06:22 -0700

    Vive setup for OpenHMD script

---
+++ prepare_Vive_OpenHMD.sh
@@ -0,0 +1,10 @@
+# Sets monitor & vive up
+xrandr --output DisplayPort-0 --mode 2160x1200 --rate 60
+xrandr --fb 4080x1200
+xrandr --output DisplayPort-0 --left-of HDMI-A-0
+
+cd ~/programs/git_repos/OpenHMD/examples/opengl/examples
+
+# from here you can test opengl or simple
+# ./simple/simple
+# ./opengl/openglexample 
