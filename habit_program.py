7f37b1563e8fa824505b3915dc3585dc61c5f627
Maximilian Van Hout <36654636+NerveCoordinator@users.noreply.github.com>
2019-06-29 06:20:18 +0000

    more cleanup

--- habit_program.py
+++ habit_program.py
@@ -1,38 +1,55 @@
-# Habit program is used for reminders of what my habits are
-# It also logs issues I've encountered.
-# I also use it to deal with recurring problems.
+'''
+Habit program is used for reminders of what my habits are
+It also logs issues I've encountered.
+I also use it to deal with recurring problems.
+
+Assumes file 'habit_focus' exists, and directory 'logs' exists
+you have to make these yourself.
+
+habit file structure:
+
+habit 1
+habit 2
+habit 3
+! problems
+problem 1
+problem 2
+problem 3
+# end
+# additional comments
+'''
 
 import time
 import datetime
 import os.path
 
-# Assumes file 'habit_focus' exists, and directory 'logs' exists
-
 # open 'habit_focus' file. Contains habits I want to develop.
 focus_file= open("habit_focus","r")
 focus_contents = focus_file.read()
 
-timestamp = time.time()
-
-# st is...? stamp time? Terrible.
-st = datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d')
-file_counter = 0
-file_path = "."
 
 # generates a name for our log by checking what already exists
 # this way if we use the program more than once a day it will
 # be numbered in order, but still have a date stamp. 
+datestamp = datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d')
+file_counter = 0
+file_path = "."
 while (os.path.exists(file_path)):
     file_counter += 1
-    file_path = st + "_" + str(file_counter)
-    if (file_counter == 1):
-       file_path = st
+    file_path = datestamp + "_" + str(file_counter)
+    if (file_counter == 1): # keeps name clean for the first file
+       file_path = datestamp
     file_path = "logs/" + file_path
 
 log_file= open(file_path,"w+")
     
 print("\n"*50)
+
+# this instruction works well, and eventually makes habits work even when they're really tricky.
 #print("The goal is to move all habits into 'integrated' and problems into 'in progress'. 2-3 ideas I can try to make that happen faster.")
+
+# more recent experimentation, trying to work on root cause analysis and reflection
+# in particular, reflecting often puts me into flow
 print("Deep thought process: address core of the problem. When I conclude, return to beginning with fresh understanding.")
 
 # goes from none -> problems -> habits
@@ -42,12 +59,12 @@ section = "none"
 # resets when we enter our problems section
 counter = 1 
 
-#habits are separated by newlines
 #first char being # signals we're done, text after that isn't read and so can be used for comments
 #first char being ! signals change of section - everything after ! on the same line is ignored
+
 for habit in focus_contents.splitlines():
     #empty line counts as being done
-    if (len(habit) < 2): 
+    if (len(habit) < 1): 
         continue
     # signals we've hit comments, so we're done
     if (habit[0] == "#"):
